c04938f0a70f70137509626d225dea98c3e8e4ce
Adam Grimm <ATGrimm@gmail.com>
2015-10-28 14:31:02 -0700

    Pseudocoded

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,5 +3,26 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  console.log('Incoming ' + req.method + ' request to ' + req.url);
+
+  //User makes a POST request
+    //is the website they are looking for present in /archives/sites?
+      //if so, redirect them to http://localhost:8080/**URL**
+        //how to redirect?
+          //window.location.href = 'http://localhost:8080/www.test.com', followed by res.end??
+          //res.end('<script>window.location.href = "http://localhost:8080/www.test.com"</script>');??
+      //hopefully this will automatically make a get request for that URL 
+    //if not
+      //add the website to the /archives/sites.txt
+      //send them to loading.html?
+
+  //User makes a GET request (by loading a subpage of the localhost)
+    //is the website they are looking for present in /archives/sites?
+     //if so, read that file and send the client that data
+    //if not, say 'we don't have that' and give them a 404
+
+
+  
+
 };
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,21 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+//this is a node server?
+
+//html fetcher will check sites.txt for any new URLs (how do we know if they are new or not?)
+  //if so, it will make a GET request to that URL
+    //when the GET request succeeds, it will store the html data to a new file in archives/sites
+  //if the GET fails
+    //save some template 404 information in archives/sites/**URL**
+  //will it then perform some operation to indicate that that site has been read from archives/sites.txt?
+  //it could add some string to the text to say its been processed
+  //or it could delete that string from the file
+
+
+
+//the fetching of data needs to be triggered by CRON.  How does this work?
+  //does the server run indefinitely and CRON triggers some function call to make it look for new URLs?
+  //or does CRON boot up the server itself, and then the default 'look for URLs' code runs?
+
+
